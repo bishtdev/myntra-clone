9d6b3f1419af5edc03cb096257d56eb7902fbae7
bishtdev <bishtdev030@gmail.com>
2024-06-06 21:59:31 +0530

    added all the functionality

---
+++ 3-myntra-react-clone/src/store/itemsSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const itemsSlice = createSlice({
+    name: "items",
+    initialState: [],
+    reducers: {
+      addIntialItems: (state, action) => {
+        return action.payload;
+      },
+    },
+});
+
+
+export const itemsActions = itemsSlice.actions
+
+export default itemsSlice
+
